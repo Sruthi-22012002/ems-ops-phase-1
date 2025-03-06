c57eec9d1927a2b83e68190cd56f3b6b51c1497b
Sruthi-22012002 <sruthivg22@gmail.com>
2025-03-06 04:24:54 +0000

    frontend

--- react-hooks-frontend/src/services/EmployeeService.js
+++ react-hooks-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const EMPLOYEE_BASE_REST_API_URL = 'http://172.212.37.29:8080/api/v1/employees';
+const EMPLOYEE_BASE_REST_API_URL = 'http://localhost:8080/api/v1/employees';
 
 class EmployeeService{
 
